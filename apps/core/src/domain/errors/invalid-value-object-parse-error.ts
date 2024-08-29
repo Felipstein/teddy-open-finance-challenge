c5222854ad675ff687c873dde6e5a2cb95104a9c
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-08-29 13:46:20 -0300

    style: fix prettier rule

--- apps/core/src/domain/errors/invalid-value-object-parse-error.ts
+++ apps/core/src/domain/errors/invalid-value-object-parse-error.ts
@@ -3,10 +3,7 @@ import ErrorCode from '@shared/error-codes';
 import DomainError from './domain-error';
 
 export default class InvalidValueObjectParseError extends DomainError {
-  constructor(
-    code = ErrorCode.INVALID_VALUE_OBJECT_PARSE,
-    message = 'Invalid value object parse',
-  ) {
+  constructor(code = ErrorCode.INVALID_VALUE_OBJECT_PARSE, message = 'Invalid value object parse') {
     super(code, message);
   }
 }
