10f52c2179af4779cab0ee12f295e287ae109890
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-08-29 14:18:45 -0300

    test: update shortened link tests to include new features

--- apps/core/src/domain/entities/shortened-link.test.ts
+++ apps/core/src/domain/entities/shortened-link.test.ts
@@ -1,4 +1,7 @@
+import DomainError from '@domain/errors/domain-error';
 import InvalidValueObjectParseError from '@domain/errors/invalid-value-object-parse-error';
+
+import { delay } from '../../../tests-utils/delay';
 
 import ShortenedLink from './shortened-link';
 
@@ -22,6 +25,31 @@ describe('ShortenedLink Entity', () => {
     expect(shortenedLink.code).toBe('mycode');
   });
 
+  it.each([
+    (shortenedLink: ShortenedLink) => {
+      shortenedLink.code = 'abcDf';
+    },
+    (shortenedLink: ShortenedLink) => {
+      shortenedLink.link = 'http://localhost:3333/new-link';
+    },
+    (shortenedLink: ShortenedLink) => {
+      shortenedLink.expiresIn = null;
+    },
+  ])('deve atualizar o campo updatedAt após sofrer uma mutação', async (updateShortenedLink) => {
+    const shortenedLink = ShortenedLink.create({
+      link: 'http://localhost:3333/original-link',
+      expiresIn: new Date(),
+    });
+
+    expect(shortenedLink.updatedAt).toEqual(shortenedLink.createdAt);
+
+    await delay();
+    updateShortenedLink(shortenedLink);
+
+    expect(shortenedLink.updatedAt).not.toEqual(shortenedLink.createdAt);
+    expect(shortenedLink.updatedAt.getTime()).toBeGreaterThan(shortenedLink.createdAt.getTime());
+  });
+
   it('deve lançar uma exception quando for criado um ShortenedLink com código inválido', () => {
     expect(() =>
       ShortenedLink.create({
@@ -29,5 +57,14 @@ describe('ShortenedLink Entity', () => {
         customCode: 'moiJWThgtrn',
       }),
     ).toThrow(InvalidValueObjectParseError);
+  });
+
+  it('deve lançar uma exception quando for tentar usar um ShortenedLink expirado', () => {
+    const shortenedLink = ShortenedLink.create({
+      link: 'http://localhost:3333/original-link',
+      expiresIn: new Date(0),
+    });
+
+    expect(() => shortenedLink.incrementUsageCount()).toThrow(DomainError);
   });
 });
