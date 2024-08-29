0382555573252fe8b5c9e12be423fcc75f71f814
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-08-29 14:09:12 -0300

    fix(domain/service): verify if length is valid

--- apps/core/src/domain/services/generate-random-code.ts
+++ apps/core/src/domain/services/generate-random-code.ts
@@ -1,5 +1,7 @@
 import domainConfig from '@config/domain-config';
+import DomainError from '@domain/errors/domain-error';
 import Code from '@domain/value-objects/code';
+import ErrorCode from '@shared/error-codes';
 import numberUtils from '@shared/utils/number-utils';
 
 const CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_@#!$*';
@@ -11,6 +13,20 @@ const randomLength = () =>
   );
 
 export default function generateRandomCode(length = randomLength()) {
+  if (length > domainConfig.valueObjects.maxCodeLength) {
+    throw new DomainError(
+      ErrorCode.CODE_TOO_LONG,
+      `Code must have at most ${domainConfig.valueObjects.maxCodeLength} characters`,
+    );
+  }
+
+  if (length < domainConfig.valueObjects.minCodeLength) {
+    throw new DomainError(
+      ErrorCode.CODE_TOO_SHORT,
+      `Code must have at least ${domainConfig.valueObjects.maxCodeLength} characters`,
+    );
+  }
+
   let code = '';
 
   for (let i = 0; i < length; i++) {
