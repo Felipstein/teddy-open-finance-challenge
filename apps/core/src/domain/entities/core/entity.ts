b67d829af775cf86b87fa526c51a41b08d3bcd66
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-08-29 12:09:35 -0300

    feat(domain): add entity component

---
+++ apps/core/src/domain/entities/core/entity.ts
@@ -0,0 +1,32 @@
+import ID from '@domain/value-objects/id';
+import objectUtils from '@shared/utils/object-utils';
+
+export default class Entity<TProps extends Record<string, any>> {
+  constructor(
+    readonly id: ID,
+    protected readonly props: TProps,
+  ) {}
+
+  /**
+   * Verifica se ambas as entidades possuem ainda os mesmos valores, não havendo nenhuma
+   * mutação.
+   */
+  equals(value: unknown) {
+    if (!value) return false;
+
+    if (value === this) return true;
+
+    if (value instanceof Entity) {
+      return this.id === value.id && objectUtils.equals(this.props, value.props);
+    }
+
+    return false;
+  }
+
+  /**
+   * Verifica se ambas as entidades são as mesmas, comparando-as pela identificação (ID).
+   */
+  same(entity: Entity<any>) {
+    return this.id === entity.id;
+  }
+}
