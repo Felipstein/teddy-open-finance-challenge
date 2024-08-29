70bcefe32f7516580592ee540a8e9cded9baf6f8
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-08-29 13:07:55 -0300

    feat: add domain configs

--- apps/core/jest.config.js
+++ apps/core/jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     '^@application/(.*)$': '<rootDir>/src/application/$1',
     '^@domain/(.*)$': '<rootDir>/src/domain/$1',
     '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
   },
   clearMocks: true,
   resetMocks: true,
---
+++ apps/core/src/config/domain-config.ts
@@ -0,0 +1,8 @@
+const domainConfig = {
+  valueObjects: {
+    minNameLength: 3,
+    maxNameLength: 50,
+  },
+};
+
+export default domainConfig;
