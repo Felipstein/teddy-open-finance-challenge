fde19852be6b72631b7010acd7473f9080cfa34a
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-08-31 15:02:58 -0300

    feat(application/http): add `string[]` value type to headers in Request

--- apps/core/src/application/http/request.ts
+++ apps/core/src/application/http/request.ts
@@ -3,7 +3,7 @@ import RequestMetadata from './request-metadata';
 import type { IncomingHttpHeaders } from 'node:http';
 
 export default interface IRequest {
-  headers: IncomingHttpHeaders & Record<string, string | undefined>;
+  headers: IncomingHttpHeaders & Record<string, string | string[] | undefined>;
   params: Record<string, string | number | undefined>;
   query: Record<string, string | string[] | number | boolean | undefined>;
   body: any;
