20e17da49081ab55251b84c526a2c176ea925d62
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-08-31 15:16:40 -0300

    refactor(infra/express): accept handler class to auto instance

--- apps/core/src/infra/http/express/adapters/adapt-handler.ts
+++ apps/core/src/infra/http/express/adapters/adapt-handler.ts
@@ -5,7 +5,15 @@ import { Handler as ExpressHandler } from 'express';
 import ExpressRequest from '../request';
 import ExpressResponse from '../response';
 
-export default function adaptHandler(handler: Handler): ExpressHandler {
+export default function adaptHandler(handler: Handler): ExpressHandler;
+export default function adaptHandler(handlerClass: new () => Handler): ExpressHandler;
+
+export default function adaptHandler(
+  handlerOrClass: Handler | (new () => Handler),
+): ExpressHandler {
+  // eslint-disable-next-line new-cap
+  const handler = typeof handlerOrClass === 'function' ? new handlerOrClass() : handlerOrClass;
+
   return async (rawRequest, rawResponse, next) => {
     if (!rawRequest.metadata) {
       rawRequest.metadata = createDefaultMetadataAttributes();
