074dd814d156ede49b70b58d68bf848d785ae2b3
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-08-31 18:08:35 -0300

    chore(is-available-code controller): check if is valid code

--- apps/core/src/application/http/components/controllers/is-available-code-controller.ts
+++ apps/core/src/application/http/components/controllers/is-available-code-controller.ts
@@ -2,6 +2,7 @@ import Handler from '@application/http/handler';
 import IRequest from '@application/http/request';
 import IShortenedLinksRepository from '@application/repositories/shortened-links-repository';
 import { Inject } from '@dependencies-hub';
+import Code from '@domain/value-objects/code';
 import createValidator from '@shared/validator';
 import { z } from 'zod';
 
@@ -17,6 +18,12 @@ export default class IsAvailableCodeController extends Handler {
 
   protected override async handle(request: IRequest) {
     const { code } = paramsValidator(request.params);
+
+    /**
+     * Checa se é um código válido
+     */
+    // eslint-disable-next-line no-new
+    new Code(code);
 
     const alreadyTaken = await this.shortenedLinksRepo.existsByCode(code);
 
